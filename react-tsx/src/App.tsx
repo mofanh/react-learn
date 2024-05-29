788f591d9a8d54bb4cee2abf15080fbfe35ed0fa
li.bojing <1325349854@qq.com>
2024-05-30 00:45:13 +0800

    优化video的宽度

--- react-tsx/src/App.tsx
+++ react-tsx/src/App.tsx
@@ -51,6 +51,17 @@ function App(props:AppProps) {
             }]
           }} onReadyPlay={undefined}/>
         </div>
+        <div>
+        <VideoJS  
+          options={{
+            autoplay: true,
+            controls: true,
+            sources: [{
+              type: "video/mp4",
+              src: "https://prod-streaming-video-msn-com.akamaized.net/a8c412fa-f696-4ff2-9c76-e8ed9cdffe0f/604a87fc-e7bc-463e-8d56-cde7e661d690.mp4",
+            }]
+          }} onReadyPlay={undefined}/>
+        </div>
     </div>
   );
 }
